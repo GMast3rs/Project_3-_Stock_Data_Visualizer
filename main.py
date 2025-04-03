239fe1c24fd98e24785b63086ce4298d5a44d7ab
Bennet Bradley <37352885+Bradlb03@users.noreply.github.com>
2025-04-02 19:52:57 -0500

    Added API connection to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from user_input import get_user_input 
-
+from api_connection import get_stock_data
 
 def main():
     
@@ -11,7 +11,20 @@ def main():
     print(f"Time Series: {user_data['time_series']}")
     print(f"Start Date: {user_data['start_date']}")
     print(f"End Date: {user_data['end_date']}")
-    
 
+    stock_data = get_stock_data(
+        user_data['stock_symbol']
+        user_data['chart_type'],
+        user_data['time_series'],
+        user_data['start_date'],
+        user_data['end_date']
+    )
+
+    if stock_data:
+        print("\nStock Data Retrieved:")
+        print(stock_data)
+    else:
+        print("\nFailed to retrieve stock data.")
+        
 if __name__ == "__main__":
     main()
